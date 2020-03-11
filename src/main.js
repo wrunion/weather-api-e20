c3b7c2a717760c2994a821bfccdebe8c97449b8f
Kent McKinney <kent.p.mckinney@gmail.com>
2020-03-10 18:35:06 -0700

    add code from curriculum for web api

--- src/main.js
+++ src/main.js
@@ -4,6 +4,28 @@ import './style.css';
 
 $(document).ready(function() {
 
-  
+  //129d36f95d0264a22fce62f61459dad0
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=129d36f95d0264a22fce62f61459dad0`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
 
 });
