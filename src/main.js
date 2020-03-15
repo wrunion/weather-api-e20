9e965137c6b16912ca943deb08917ce441e9a68a
wrunion <winterrunion@gmail.com>
2020-03-15 14:49:17 -0700

    refactor api code to use async and await

--- src/main.js
+++ src/main.js
@@ -8,63 +8,29 @@ $(document).ready(function() {
     let city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=129d36f95d0264a22fce62f61459dad0`)
-      .then(function(response) {
-        if (response.ok && response.status ==200) {
-          return response.json();
+    (async () => {
+      try {
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=129d36f95d0264a22fce62f61459dad0`);
+        let jsonifiedResponse;
+        if (response.ok && response.status === 200) {
+          jsonifiedResponse = await response.json();
         } else {
-          return false;
+          jsonifiedResponse = false;
         }
-      })
-      .catch(function(error) {
-        return false;
-      })
-      .then(function(jsonifiedResponse) {
         getElements(jsonifiedResponse);
-      });
+      } catch(e) {
+        getElements(false);
+      }
+    })();
 
-      const getElements = function(response) {
-        if (response) {
-          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-        } else {
-          $('.showHumidity').text(`There was an error handling your request.`);
-          $('.showTemp').text(`Please check your inputs and try again!`);
-        }
-      };
-    });
+    const getElements = function(response) {
+      if (response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      } else {
+        $('.showHumidity').text(`There was an error handling your request.`);
+        $('.showTemp').text(`Please check your inputs and try again!`);
+      }
+    };
   });
-
-// $(document).ready(function() {
-
-//   //129d36f95d0264a22fce62f61459dad0
-//   $('#weatherLocation').click(function() {
-//     const city = $('#location').val();
-//     $('#location').val("");
-
-//     let ourPromise = new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=129d36f95d0264a22fce62f61459dad0`;
-
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(Error(request.statusText));
-//         }
-//       };
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-    
-//     ourPromise.then(function(response) {
-//       let body = JSON.parse(response);
-//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-//     }, function(error) {
-//       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-
-//     };
-//   });
-
-// });
+});
